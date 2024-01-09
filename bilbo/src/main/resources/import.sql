81e2e4c36750de3d04df29136fa0f49d7f90e02c
Holler, Patrik <hollepat@fel.cvut.cz>
2024-01-09 17:21:44 +0100

    updated import.sql

--- bilbo/src/main/resources/import.sql
+++ bilbo/src/main/resources/import.sql
@@ -30,3 +30,14 @@ CREATE TABLE battles (
          CONSTRAINT fk_battle_groupA FOREIGN KEY (groupA_id) REFERENCES groups(id),
          CONSTRAINT fk_battle_groupB FOREIGN KEY (groupB_id) REFERENCES groups(id)
 );
+
+INSERT INTO heroes (hero_name, belief, strength, magic, will, group_id) VALUES ('Gandalf', 'GOOD', 2, 5, 10, 1);
+INSERT INTO heroes (hero_name, belief, strength, magic, will, group_id) VALUES ('Bilbo', 'GOOD', 3, 10, 1, 1);
+INSERT INTO heroes (hero_name, belief, strength, magic, will, group_id) VALUES ('Thorin', 'GOOD', 10, 5, 0, 1);
+
+INSERT INTO heroes (hero_name, belief, strength, magic, will, group_id) VALUES ('Bolg', 'EVIL', 10, 10, 0, 2);
+INSERT INTO heroes (hero_name, belief, strength, magic, will, group_id) VALUES ('Golfimbul', 'EVIL', 6, 4, 0, 2);
+INSERT INTO heroes (hero_name, belief, strength, magic, will, group_id) VALUES ('Necromancer', 'EVIL', 3, 3, 10, 2);
+
+INSERT INTO groups (name, groupBelief) VALUES ('Group 1', 'BAD');
+INSERT INTO groups (name, groupBelief) VALUES ('Group 2', 'GOOD');
