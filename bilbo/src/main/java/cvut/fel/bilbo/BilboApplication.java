9c4fbf1f14ad50d25bbe6202a89fb826c49d7a6a
Holler, Patrik <hollepat@fel.cvut.cz>
2024-01-09 09:47:48 +0100

    added dependecies

---
+++ bilbo/src/main/java/cvut/fel/bilbo/BilboApplication.java
@@ -0,0 +1,13 @@
+package cvut.fel.bilbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilboApplication.class, args);
+    }
+
+}
---
+++ bilbo/src/test/java/cvut/fel/bilbo/BilboApplicationTests.java
@@ -0,0 +1,13 @@
+package cvut.fel.bilbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
